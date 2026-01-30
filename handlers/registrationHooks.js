ae500d6e7d340846f986343229a298a7aad7ce95
Sara Kazemi <sara@kazemicode.org>
2026-01-29 16:18:00 -0800

    Update registrationHooks.js

--- handlers/registrationHooks.js
+++ handlers/registrationHooks.js
@@ -20,10 +20,10 @@ let body;
 
 /**
  *
- * Inline hook handler for registration with demoble scenarios determined by email domain
+ * Inline hook handler for registration with demoble scenarios determined by invite code
  *
  **/
-router.post("/domain", function (req, res) {
+router.post("/invite", function (req, res) {
   const payload = req.body;
 
   // Make sure we have a valid request payload
@@ -71,18 +71,10 @@ router.post("/domain", function (req, res) {
     return;
   }
 
-  // Parse user email from request payload
+  // Parse user invite_code from request payload
   const userProfile = payload.data.userProfile;
-  const emailAddress = userProfile.email;
-  const emailName = emailAddress.substring(0, emailAddress.indexOf("@")); // email username
-  const emailDomain = helpers.parseEmailDomain(emailAddress); // email domain
-  const emailPrefix = emailDomain.substring(0, emailDomain.indexOf(".")); // email prefix (e.g., allow, deny, error)
-  const parsedEmail = helpers.parseEmail(
-    emailAddress,
-    emailDomain,
-    emailPrefix,
-    emailName
-  );
+  const invite_code = userProfile.invite_code;
+
 
   title = req.originalUrl;
   description = `Below is the <b>request</b> that Okta sent to our Registration Hook`;
@@ -90,15 +82,15 @@ router.post("/domain", function (req, res) {
 
   hookViewer.emitViewerEvent(title, description, body, false);
 
-  // *** DEMO *** depending on the email domain provided in the registration form,
+  // *** DEMO *** depending on the validity of the invite code,
   // this API will perform different actions
-  switch (emailPrefix) {
+  switch (invite_code) {
     case "allow":
       commands = [
         {
-          type: "com.okta.user.profile.update",
+          type: "com.okta.action.update",
           value: {
-            email: parsedEmail,
+            registration: "ALLOW",
           },
         },
       ];
@@ -125,20 +117,14 @@ router.post("/domain", function (req, res) {
         errorSummary: "Registration Denied",
         errorCauses: [
           {
-            errorSummary:
-              "Invalid email domain: " +
-              emailDomain +
-              " (message from inline hook)",
-            reason: "INVALID_EMAIL_DOMAIN",
-            locationType: "body",
-            location: "email",
-            domain: emailDomain,
+            errorSummary: "Invalid invite code " + "(message from inline hook)",
+            reason: "INVALID_INVITE_CODE",
           },
         ],
       };
       contextMessage = {
         status: "Registration Failed",
-        reason: "Registration denied for invalid email domain: " + emailDomain,
+        reason: "Invalid invite code",
       };
       debugContext = {
         contextMessage: JSON.stringify(contextMessage),
